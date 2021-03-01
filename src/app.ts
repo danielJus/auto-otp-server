3a29c2ff7a15311b032e448a55e2f1d2710e3ede
Daniel <danieljust2395@gmail.com>
2021-03-01 09:15:12 -0600

    s

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
